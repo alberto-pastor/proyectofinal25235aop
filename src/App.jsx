948dc606255683f44bdb9943156e259aa5db610b
Alberto <apastor63@hotmail.com>
2025-10-20 21:30:04 -0300

    Fix: correct casing for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
-import Ofertas from './components/Ofertas';
-import Infaltables from './components/Infaltables';
+//import Ofertas from './components/Ofertas';
+//import Infaltables from './components/Infaltables';
 import Login from './components/Login'; 
 import Footer from './components/Footer'
 
@@ -14,8 +14,8 @@ function App() {
       <Routes>
         <Route path="/administracion" element={<Login />} />
         <Route path="/" element={<Home />} />
-        <Route path="/ofertas" element={<Ofertas />} />
-        <Route path="/infaltables" element={<Infaltables />} />
+        //<Route path="/ofertas" element={<Ofertas />} />
+        //<Route path="/infaltables" element={<Infaltables />} />
       </Routes>
       <Footer/>
     </Router>
